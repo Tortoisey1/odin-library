ad92ede78ac58e570474ea0a281a3dd1709e140a
Tortoisey1 <88613481+Tortoisey1@users.noreply.github.com>
2024-06-23 22:49:18 +0800

    Built up the skeleton files

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(name) {
+    // the constructor...
+    this.name = name;
+}
+
+function addBookToLibrary(name) {
+    temp = new Book(name);
+    myLibrary.push(temp);
+    console.log(myLibrary);
+}
+
+function displayLibrary() {
+    array.forEach(element => {
+
+    });
+}
