c6066ed6b931acd25d39829b7e7520e1c87d90bf
Tortoisey1 <88613481+Tortoisey1@users.noreply.github.com>
2024-06-24 18:52:48 +0800

    Added the sidebar with an incomplete submit button function

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const myLibrary = [];
+const myLibrary = [new Book('Harry Potter'),
+new Book('The Giver'),
+new Book('Lord of the Rings')];
+
+const libraryDiv = document.querySelector('.library');
+
+const newBookButton = document.querySelector('.newBook');
+
+const sideBarDiv = document.querySelector('.sidebar');
+
+const formSubmit = document.getElementById('submit-form');
+
 
 function Book(name) {
     // the constructor...
@@ -6,13 +17,33 @@ function Book(name) {
 }
 
 function addBookToLibrary(name) {
-    temp = new Book(name);
-    myLibrary.push(temp);
+    const newBook = new Book(name);
+    myLibrary.push(newBook);
     console.log(myLibrary);
 }
 
 function displayLibrary() {
-    array.forEach(element => {
-
+    myLibrary.forEach(book => {
+        const bookDiv = document.createElement('div');
+        bookDiv.textContent = book.name;
+        libraryDiv.appendChild(bookDiv);
     });
 }
+
+
+function displayForm() {
+    sideBarDiv.style.display = 'flex';
+}
+
+
+function submitForm(event) {
+    let warn = "preventDefault() won't let you check this!<br>";
+    formSubmit.innerHTML += warn;
+    event.preventDefault();
+}
+
+newBookButton.addEventListener('click', displayForm);
+
+formSubmit.addEventListener('click', submitForm, false);
+
+displayLibrary();
